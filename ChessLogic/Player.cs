459dfdd163deb51bf243977ab3f0aebefc24d837
Mark Aguila <markaguila1@gmail.com>
2026-04-16 16:27:32 -0400

    Initial project
    Board and rook, bishop, queen implementation

---
+++ ChessLogic/Player.cs
@@ -0,0 +1,22 @@
+﻿namespace ChessLogic
+{
+    public enum Player
+    {
+        None, 
+        White,
+        Black
+    }
+
+    public static class PlayerExtensions
+    {
+        public static Player Opponent(this Player player)
+        {
+            return player switch
+            {
+                Player.White => Player.Black,
+                Player.Black => Player.Black,
+                _ => Player.None,
+            };
+        }
+    }
+}
